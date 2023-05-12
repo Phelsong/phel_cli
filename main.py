f575604c569ff9939ba7f8a631424cf74c41f00a
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2023-05-12 16:04:18 -0500

    misc

---
+++ main.py
@@ -0,0 +1,45 @@
+# libs
+import typer
+import rich
+import sys
+from pathlib import Path
+
+# imports
+
+# =========================
+app = typer.Typer()
+state = {"verbose": False}
+# =========================
+
+
+@app.callback()
+def main():
+    #arg parse
+    pass
+# ========================
+
+@app.command("help")
+    print("help")
+
+# ========================
+@app.command("hello")
+def hello():
+    print(":wave:")
+
+
+# ========================
+
+
+# app.add_typer(other_app, name="other")
+
+
+# @other_app.command("hello")
+# def other_hello():
+#    print("other")
+
+
+# =======================
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    app(args)
